a53b8c860f47158b5ac565088fc1f5b544b9590d
HeOfLittleSleep <pottim99@gmail.com>
2023-07-16 00:11:08 -0500

    Added UAC prompt
    Added the ability to detect admin status and promt the user for elevation if necessary. code shamelessly copied from stackoverflow.
    
    was doing other things and decided to use this script, got annoyed when it reee'd about access denied, so I went on a slight side-quest to change this this. subject to change

--- symGenTK.py
+++ symGenTK.py
@@ -2,6 +2,7 @@ from tkinter import *
 #from tkinter import filedialog
 import os
 #import win32com.shell.shell as shell
+import ctypes, sys
 
 def generate_symlink():
   symName = strSymName.get()
@@ -25,34 +26,46 @@ def generate_symlink():
    #for i in input:
       #print(i)
   
-mainWindow = Tk()
-mainWindow.geometry("250x350")
-mainWindow.title("SymLink Generator")
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+if is_admin():
+    mainWindow = Tk()
+    mainWindow.geometry("250x350")
+    mainWindow.title("SymLink Generator")
  
-symLNameLabel = Label(text = "Symlink Name",)
-symLTargetLabel = Label(text = "SymLink Target",)
-symLLocationLabel = Label(text = "Symlink Location",)
-symLNameLabel.place(x = 15, y = 10)
-symLTargetLabel.place(x = 15, y = 55)
-symLLocationLabel.place(x = 15, y = 100)
+    symLNameLabel = Label(text = "Symlink Name",)
+    symLTargetLabel = Label(text = "SymLink Target",)
+    symLLocationLabel = Label(text = "Symlink Location",)
+    symLNameLabel.place(x = 15, y = 10)
+    symLTargetLabel.place(x = 15, y = 55)
+    symLLocationLabel.place(x = 15, y = 100)
 
 
-strSymName = StringVar()
-strSymTarget = StringVar()
-strSymLocation = StringVar()
+    strSymName = StringVar()
+    strSymTarget = StringVar()
+    strSymLocation = StringVar()
 
-symLNameEntry = Entry(textvariable = strSymName, width = "30")
-symLTargetEntry = Entry(textvariable = strSymTarget, width = "30")
-symLLocationEntry = Entry(textvariable = strSymLocation, width = "30")
+    symLNameEntry = Entry(textvariable = strSymName, width = "30")
+    symLTargetEntry = Entry(textvariable = strSymTarget, width = "30")
+    symLLocationEntry = Entry(textvariable = strSymLocation, width = "30")
 
-symLNameEntry.place(x = 15, y = 30)
-symLTargetEntry.place(x = 15, y = 75)
-symLLocationEntry.place(x = 15, y = 120)
+    symLNameEntry.place(x = 15, y = 30)
+    symLTargetEntry.place(x = 15, y = 75)
+    symLLocationEntry.place(x = 15, y = 120)
 
-genSym = Button(mainWindow,text = "Generate SymLink", width = "30", height = "2", command = generate_symlink, bg = "grey")
-genSym.place(x = 15, y = 290)
+    genSym = Button(mainWindow,text = "Generate SymLink", width = "30", height = "2", command = generate_symlink, bg = "grey")
+    genSym.place(x = 15, y = 290)
 
-#browseTarget = Button(mainWindow, text ='...', command = lambda:file_opener())
-#browseTarget.place()
+    #browseTarget = Button(mainWindow, text ='...', command = lambda:file_opener())
+    #browseTarget.place()
 
-mainWindow.mainloop()
+    mainWindow.mainloop()
+
+else:
+    # Re-run the program with admin rights
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+
